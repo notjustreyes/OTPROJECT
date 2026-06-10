4eaa8adf489d1b198ea091ab40fc0112ab654d6b
notjustreyes <dotimasjustine@gmail.com>
2026-06-11 00:40:03 +0800

    added rotemit airy blue/dragon fruit

--- products.js
+++ products.js
@@ -19,10 +19,20 @@ const inventoryData = [
 
   {
     sku: "1183C659-250",
-    name: "EDR 78",
+    name: "EDR 78 (SINGAPORE EXCLUSIVE)",
     colour: "beige/beet juice",
     image: "https://images.asics.com/is/image/asics/1183C659_250_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
     stock: "NIL",
     location: "NIL"
+  },
+
+  {
+	  sku: "1183C614-400",
+	  name: "ROTEMIT",
+	  colour: "airy blue/dragon fruit",
+	  image: "https://images.asics.com/is/image/asics/1183C614_400_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
+	  stock: "NIL",
+	  location: "NIL"
   }
+
 ];
