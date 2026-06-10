1f3603ac0ff148c6e09406f18c78707d78752023
notjustreyes <dotimasjustine@gmail.com>
2026-06-11 01:03:23 +0800

    added M66 yellow/black STD/SD/NM/PF

--- products.js
+++ products.js
@@ -2,7 +2,7 @@
 const inventoryData = [
   {
     sku: "1183B603-202",
-    name: "Mexico 66 SLIP-ON",
+    name: "MEXICO 66 SLIP-ON",
     colour: "dark brown/dark brown",
     image: "https://images.asics.com/is/image/asics/1183B603_202_SR_RT_GLB?qlt=100&wid=1280&hei=1452&bgc=255,255,255&resMode=bisharp",
     stock: "NIL",
@@ -10,7 +10,7 @@ const inventoryData = [
   },
   {
     sku: "1183C123-202",
-    name: "Mexico 66 SABOT",
+    name: "MEXICO 66 SABOT",
     colour: "dark brown/dark brown",
     image: "https://images.asics.com/is/image/asics/1183C123_202_SR_RT_GLB?qlt=100&wid=1280&hei=1452&bgc=255,255,255&resMode=bisharp",
     stock: "NIL",
@@ -31,6 +31,41 @@ const inventoryData = [
 	  name: "ROTEMIT",
 	  colour: "airy blue/dragon fruit",
 	  image: "https://images.asics.com/is/image/asics/1183C614_400_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
+	  stock: "NIL",
+	  location: "NIL"
+  },
+
+  {
+	  sku: "1183C102-751",
+	  name: "MEXICO 66",
+	  colour: "Yellow/Black",
+	  image: "https://images.asics.com/is/image/asics/1183C102_751_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
+	  stock: "NIL",
+	  location: "NIL"
+  },
+  {
+	  sku: "1183C319-750",
+	  name: "MEXICO 66 NM",
+	  colour: "Yellow/Black",
+	  image: "https://images.asics.com/is/image/asics/1183C319_750_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
+	  stock: "NIL",
+	  location: "NIL"
+  },
+
+  {
+	  sku: "1183A872-752",
+	  name: "MEXICO 66 SD",
+	  colour: "Yellow/Black",
+	  image: "https://images.asics.com/is/image/asics/1183A872_752_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
+	  stock: "NIL",
+	  location: "NIL"
+  },
+
+  {
+	  sku: "1183C621-750",
+	  name: "MEXICO 66 PF",
+	  colour: "YELLOW/BLACK",
+	  image: "https://images.asics.com/is/image/asics/1183C621_750_SR_RT_GLB?qlt=100&wid=1024&hei=768&bgc=255,255,255&resMode=bisharp",
 	  stock: "NIL",
 	  location: "NIL"
   }
